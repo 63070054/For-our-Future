0fa83e8492590f9a2ce9537dfad222ac8b8587d7
63070134 <73682774+63070134@users.noreply.github.com>
2022-04-10 14:31:23 +0700

    CardRound

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 import CardNews from '@/components/CardNews.vue'
+import CardRound from '@/components/CardRound.vue'
 </script>
 
 <template>
 
   <NavBar></NavBar>
   <CardNews />
+  <CardRound />
   <!-- <RouterView /> -->
   
 </template>
