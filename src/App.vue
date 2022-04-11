072fc7803262c48be30402f45f5f2f9d3291c68a
63070134 <73682774+63070134@users.noreply.github.com>
2022-04-11 15:15:59 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-<<<<<<< HEAD
 import CardNews from '@/components/CardNews.vue'
 import CardRound from '@/components/CardRound.vue'
-=======
->>>>>>> d1c8da992d24cb8f3c160e3d26a92ef5ae7c46b6
 </script>
 
 <template>
 
   <NavBar></NavBar>
-<<<<<<< HEAD
-  <CardNews />
-  <CardRound />
+  <!-- <CardNews /> -->
+  <!-- <CardRound /> -->
   <!-- <RouterView /> -->
-=======
   <RouterView />
->>>>>>> d1c8da992d24cb8f3c160e3d26a92ef5ae7c46b6
   
 </template>
 
